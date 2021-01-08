fc65de5dadfc8e11922358d4ae01a34f8b6132c4
ShenYe233 <327716943@qq.com>
2021-01-08 15:53:32 +0800

    update

--- src/main/java/nightcat/plugin/godgame/GodGameCommand.kt
+++ src/main/java/nightcat/plugin/godgame/GodGameCommand.kt
@@ -29,4 +29,44 @@ class GodGameCommand : BaseMainCommand(),Helper {
         }
     }
 
+    @SubCommand(description = "查询变量")
+    var look: BaseSubCommand = object : BaseSubCommand() {
+        override fun getArguments(): Array<Argument> {
+            return arrayOf(
+                    Argument("玩家"),
+                    Argument("变量类型") { listOf("Character", "Language") },
+                    //Argument("数量")
+            )
+        }
+        override fun onCommand(sender: CommandSender, command: Command, s: String, args: Array<String>) {
+            val player = Bukkit.getPlayerExact(args[0])
+            if (player == null) {
+                sender.error("§7目标 §f${args[0]} §7离线.")
+                return
+            }
+            sender.info("&7目标的 &f${args[1]} &7值为 &f${Tools.getIntegral(player,args[1],"不存在")}")
+            //sender.info("&7目标的 &f${args[1]} &7设置为 &f${args[2]}.")
+        }
+    }
+
+    @SubCommand(description = "扣除变量")
+    var take: BaseSubCommand = object : BaseSubCommand() {
+        override fun getArguments(): Array<Argument> {
+            return arrayOf(
+                    Argument("玩家"),
+                    Argument("变量类型") { listOf("Character", "Language") },
+                    Argument("数量")
+            )
+        }
+        override fun onCommand(sender: CommandSender, command: Command, s: String, args: Array<String>) {
+            val player = Bukkit.getPlayerExact(args[0])
+            if (player == null) {
+                sender.error("§7目标 §f${args[0]} §7离线.")
+                return
+            }
+            //sender.info("&7目标的 &f${args[1]} &7值为 &f${Tools.getIntegral(player,args[1],"不存在")}")
+            Tools.takeIntegral(player,args[1],args[2].toInt())
+
+        }
+    }
 }
