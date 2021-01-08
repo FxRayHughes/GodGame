138a05d5834c251373ccfb31bf95c4c8abb00775
Ray_Hughes <1523165110@qq.com>
2021-01-08 17:06:51 +0800

    2333...

--- src/main/java/nightcat/plugin/godgame/GGListener.kt
+++ src/main/java/nightcat/plugin/godgame/GGListener.kt
@@ -1,25 +1,33 @@
 package nightcat.plugin.godgame
 
 import io.izzel.taboolib.module.inject.TListener
+import org.bukkit.Bukkit
 import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
+import java.util.regex.Pattern
 
 @TListener
 class GGListener : Listener {
 
     @EventHandler
-    fun onAsyncPlayerChatEvent(event: AsyncPlayerChatEvent){
-       if(!(Tools.clockMap[event.player] == true)) {
-           return
-       }
-        if(event.message == "gg"){
-
+    fun onAsyncPlayerChatEvent(event: AsyncPlayerChatEvent) {
+        if (Tools.clockMap[event.player] != true && !event.message.regex()) {
+            return
         }
+        val keys = GodGame.settings.getConfigurationSection("CapturedGiveing")?.getKeys(false) ?: return
+
+
     }
 
-    fun regex(){
-
+    private fun String.regex(): Boolean {
+        val list = GodGame.settings.getStringList("CapturedWorld")
+        list.forEach {
+            if (Pattern.compile(it).matcher(this).matches()) {
+                return true
+            }
+        }
+        return false
     }
 }
--- src/main/java/nightcat/plugin/godgame/GodGameCommand.kt
+++ src/main/java/nightcat/plugin/godgame/GodGameCommand.kt
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player
 import org.bukkit.permissions.PermissionDefault
 
 @BaseCommand(name = "karma", permissionDefault = PermissionDefault.OP)
-class GodGameCommand : BaseMainCommand(),Helper {
+class GodGameCommand : BaseMainCommand(), Helper {
 
     @SubCommand(description = "设置变量")
     var set: BaseSubCommand = object : BaseSubCommand() {
@@ -19,6 +19,7 @@ class GodGameCommand : BaseMainCommand(),Helper {
                 Argument("数量")
             )
         }
+
         override fun onCommand(sender: CommandSender, command: Command, s: String, args: Array<String>) {
             val player = Bukkit.getPlayerExact(args[0])
             if (player == null) {
@@ -26,11 +27,11 @@ class GodGameCommand : BaseMainCommand(),Helper {
                 return
             }
             val compare = listOf("Character", "Language")
-            if(!compare.contains(args[1])){
+            if (!compare.contains(args[1])) {
                 sender.error("&7您输入的参数不符合. 请输入&f‘Character’&7 或 &f‘Language’ &7.")
                 return
             }
-            Tools.setIntegral(player,args[1],args[2])
+            Tools.setIntegral(player, args[1], args[2])
             sender.info("&7目标的 &f${args[1]} &7设置为 &f${args[2]}.")
         }
     }
@@ -39,11 +40,12 @@ class GodGameCommand : BaseMainCommand(),Helper {
     var look: BaseSubCommand = object : BaseSubCommand() {
         override fun getArguments(): Array<Argument> {
             return arrayOf(
-                    Argument("玩家"),
-                    Argument("变量类型") { listOf("Character", "Language") },
-                    //Argument("数量")
+                Argument("玩家"),
+                Argument("变量类型") { listOf("Character", "Language") },
+                //Argument("数量")
             )
         }
+
         override fun onCommand(sender: CommandSender, command: Command, s: String, args: Array<String>) {
             val player = Bukkit.getPlayerExact(args[0])
             if (player == null) {
@@ -51,11 +53,11 @@ class GodGameCommand : BaseMainCommand(),Helper {
                 return
             }
             val compare = listOf("Character", "Language")
-            if(!compare.contains(args[1])){
+            if (!compare.contains(args[1])) {
                 sender.error("&7您输入的参数不符合. 请输入&f‘Character’&7 或 &f‘Language’ &7.")
                 return
             }
-            sender.info("&7目标的 &f${args[1]} &7值为 &f${Tools.getIntegral(player,args[1],"不存在")}")
+            sender.info("&7目标的 &f${args[1]} &7值为 &f${Tools.getIntegral(player, args[1], "不存在")}")
             //sender.info("&7目标的 &f${args[1]} &7设置为 &f${args[2]}.")
         }
     }
@@ -64,11 +66,12 @@ class GodGameCommand : BaseMainCommand(),Helper {
     var take: BaseSubCommand = object : BaseSubCommand() {
         override fun getArguments(): Array<Argument> {
             return arrayOf(
-                    Argument("玩家"),
-                    Argument("变量类型") { listOf("Character", "Language") },
-                    Argument("数量")
+                Argument("玩家"),
+                Argument("变量类型") { listOf("Character", "Language") },
+                Argument("数量")
             )
         }
+
         override fun onCommand(sender: CommandSender, command: Command, s: String, args: Array<String>) {
             val player = Bukkit.getPlayerExact(args[0])
             if (player == null) {
@@ -76,30 +79,35 @@ class GodGameCommand : BaseMainCommand(),Helper {
                 return
             }
             val compare = listOf("Character", "Language")
-            if(!compare.contains(args[1])){
+            if (!compare.contains(args[1])) {
                 sender.error("&7您输入的参数不符合. 请输入&f‘Character’&7 或 &f‘Language’ &7.")
                 return
             }
             //sender.info("&7目标的 &f${args[1]} &7值为 &f${Tools.getIntegral(player,args[1],"不存在")}")
-            Tools.takeIntegral(player,args[1],args[2].toInt())
+            Tools.takeIntegral(player, args[1], args[2].toInt())
         }
     }
 
     @SubCommand(description = "enable")
-    val enable: BaseSubCommand = object : BaseSubCommand(){
+    val enable: BaseSubCommand = object : BaseSubCommand() {
         override fun getArguments(): Array<Argument> {
             return arrayOf(
-                    Argument("玩家")
+                Argument("玩家")
             )
         }
 
         override fun onCommand(sender: CommandSender, command: Command, s: String, args: Array<String>) {
             val player = Bukkit.getPlayer(args[0])
-            if(player == null){
+            if (player == null) {
                 sender.error("&7目标 &f${args[0]} &7离线.")
             }
-            Tools.clockMap.put(player ?: return,true)
-
+            Tools.clockMap[player ?: return] = true
+            val time = GodGame.settings.getInt("CapturedTime", 200)
+            Bukkit.getScheduler().runTaskLater(GodGame.plugin, Runnable {
+                if (Tools.clockMap[player] != null){
+                    Tools.clockMap.remove(player)
+                }
+            }, time.toLong())
         }
     }
 }
