2438d95b531680f6118c7bf6bafbcfe5584da246
Ray_Hughes <1523165110@qq.com>
2021-01-08 15:27:50 +0800

    2333...

---
+++ src/main/java/nightcat/plugin/godgame/GodGameCommand.kt
@@ -0,0 +1,32 @@
+package nightcat.plugin.godgame
+
+import io.izzel.taboolib.module.command.base.*
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.permissions.PermissionDefault
+
+@BaseCommand(name = "karma", permissionDefault = PermissionDefault.OP)
+class GodGameCommand : BaseMainCommand(),Helper {
+
+    @SubCommand(description = "设置变量")
+    var set: BaseSubCommand = object : BaseSubCommand() {
+        override fun getArguments(): Array<Argument> {
+            return arrayOf(
+                Argument("目标"),
+                Argument("变量类型") { listOf("Character", "Language") },
+                Argument("数量")
+            )
+        }
+        override fun onCommand(sender: CommandSender, command: Command, s: String, args: Array<String>) {
+            val player = Bukkit.getPlayerExact(args[0])
+            if (player == null) {
+                sender.error("§7目标 §f${args[0]} §7离线.")
+                return
+            }
+            Tools.setIntegral(player,args[1],args[2])
+            sender.info("&7目标的 &f${args[1]} &7设置为 &f${args[2]}.")
+        }
+    }
+
+}
---
+++ src/main/java/nightcat/plugin/godgame/Helper.kt
@@ -0,0 +1,66 @@
+package nightcat.plugin.godgame
+
+import io.izzel.taboolib.Version
+import io.izzel.taboolib.module.inject.TInject
+import io.izzel.taboolib.module.locale.TLocale
+import io.izzel.taboolib.util.lite.cooldown.Cooldown
+import org.bukkit.Effect
+import org.bukkit.FluidCollisionMode
+import org.bukkit.Material
+import org.bukkit.Sound
+import org.bukkit.block.Block
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+interface Helper {
+
+    fun Player.getTargetBlockExact(): Block? {
+        return if (Version.isAfter(Version.v1_13)) {
+            this.getTargetBlockExact(10, FluidCollisionMode.NEVER)
+        } else {
+            this.getTargetBlock(setOf(Material.AIR), 10)
+        }
+    }
+
+    fun CommandSender.info(value: String) {
+        this.sendMessage("§8[§f GodGame §8] §7${value.replace("&", "§")}")
+        if (this is Player && !Global.cd.isCooldown(this.name)) {
+            this.playSound(this.location, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 2f)
+        }
+    }
+
+    fun CommandSender.error(value: String) {
+        this.sendMessage("§8[§c GodGame §8] §7${value.replace("&", "§")}")
+        if (this is Player && !Global.cd.isCooldown(this.name)) {
+            this.playSound(this.location, Sound.ENTITY_VILLAGER_NO, 1f, 1f)
+        }
+    }
+
+    fun Player.info(value: String) {
+        this.sendMessage("§8[§f GodGame §8] §7${value.replace("&", "§")}")
+        if (!Global.cd.isCooldown(this.name)) {
+            this.playSound(this.location, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 2f)
+        }
+    }
+
+    fun Player.error(value: String) {
+        this.sendMessage("§8[§c GodGame §8] §7${value.replace("&", "§")}")
+        if (!Global.cd.isCooldown(this.name)) {
+            this.playSound(this.location, Sound.ENTITY_VILLAGER_NO, 1f, 1f)
+        }
+    }
+
+    fun Block.display() {
+        world.playEffect(location, Effect.STEP_SOUND, type)
+    }
+
+    fun String.unColored(): String {
+        return TLocale.Translate.setUncolored(this)
+    }
+
+    object Global {
+
+        @TInject
+        val cd = Cooldown("command.sound", 50)
+    }
+}
